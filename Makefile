d7c2c2ff6af765c5c75b84e211451375677f3706
yinhui-evelyn <71414819+evelynliyh@users.noreply.github.com>
2021-04-09 01:12:12 -0700

    added basic structure and modified the Makefile

--- Makefile
+++ Makefile
@@ -1,1 +1,6 @@
+sql5300: sql5300.o
 
+sql5300.o: sql5300.cpp
+
+clean:
+		rm -f sql5300.o sql5300
--- sql5300.cpp
+++ sql5300.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <cassert>
 #include "db_cxx.h"
 
 /*
@@ -17,7 +20,7 @@ exec
 */
 
 
-
+using namespace hsql;
 using namespace std;
 
 int main(int len, char* args[])
@@ -28,7 +31,50 @@ int main(int len, char* args[])
         return 1;
     }
 
+    // need to check db existance?
+    char* directory = args[1];
+    cout << "sql5300 BerkDB environment at" << directory << endl;
 
+    DbEnv env(0U);
+	env.set_message_stream(&std::cout);
+	env.set_error_stream(&std::cerr);
+    // try catch?
+	env.open(envdir.c_str(), DB_CREATE | DB_INIT_MPOOL, 0);
+
+    _DB_ENV = &env;
    
+    // prompt SQL
+    while (true)
+    {
+        cout << "SQL> ";
+        string query = "";
+        cin >> query;
+        cout << endl;
+
+        // query with nothing
+        if(query.length() == 0) 
+            continue; 
+        if(query == "quit")
+            break:
+        // test?
+
+    }
+
     return EXIT_SUCCESS;
 }
+
+// expressionToString: symbols *, ., ?,  
+
+// operatorToString: and, or, not
+
+// columnDefToString: DOUBLE, INT, TEXT
+
+// tableRefToString: join, cross
+
+// create table
+
+// select
+
+// insert
+
+// execute select/insert/create
