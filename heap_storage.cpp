387836727b73df57f5907a47f2d73b143fad1aa2
toxuxu <tong.tx.xu@oracle.com>
2021-04-19 10:00:10 -0700

    add test code for Heap storage below functions are tested: create table; drop table; create table if not exist; select; project;

--- heap_storage.cpp
+++ heap_storage.cpp
@@ -390,11 +390,59 @@ Dbt* HeapTable::marshal(const ValueDict *row)
 // !! not finish
 ValueDict* HeapTable::unmarshal(Dbt *data)
 {
-    map<, > row;
-    int offset = 0;
-    for(Identifier column_name: this->column_names)
-    {
-        this->co
+    for (auto const& column_name : this->column_names) {
+
+
+
+
+
+
+
+
     }
     
+}
+
+// below method is from: https://seattleu.instructure.com/courses/1597073/files/66811759
+// the object in this test code is not immutable. TODO: fix it
+bool test_heap_storage() {
+    ColumnNames column_names;
+    column_names.push_back("a");
+    column_names.push_back("b");
+    ColumnAttributes column_attributes;
+    ColumnAttribute ca(ColumnAttribute::INT);
+    column_attributes.push_back(ca);
+    ca.set_data_type(ColumnAttribute::TEXT);
+    column_attributes.push_back(ca);
+
+    HeapTable table1("_test_create_drop_cpp", column_names, column_attributes);
+    table1.create();
+    std::cout << "create ok" << std::endl;
+    table1.drop();
+    std::cout << "drop ok" << std::endl;
+
+    HeapTable table("_test_data_cpp", column_names, column_attributes);
+    table.create_if_not_exists();
+    std::cout <<"create_if_not_exists_ok" << std::endl;
+
+    ValueDict row;
+    row["a"] = Value(12);
+    row["b"] = Value("Hello!");
+    std::cout << "try insert" << std::endl;
+    table.insert(&row);
+    std::cout << "insert ok" << std::endl;
+
+    Handles *handles = table.select();
+    std::cout << "select ok " << handles->size() << std::endl;
+    ValueDict *result = table.project((*handles)[0]);
+    std::cout << "project ok" << std::endl;
+    Value value = (*result)["a"];
+    if (value.n != 12) 
+        return false;
+    value = (*result)["b"];
+    if (value.s != "Hello!")
+        return false;
+    table.drop();
+
+    return true;
 }
